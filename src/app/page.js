9def420e5f7aff6850a689056b2cba01d056e71a
Kunal W <kunalw2002@gmail.com>
2024-04-14 18:25:58 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -131,6 +131,178 @@ const Home = () => {
 
 
 				<div className="currentPage section-divider">
+					<span>KUNULL / CERTIFICATIONS ►</span>
+				</div>
+			
+
+				<p className='spacing'>
+					<ul>
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="cipher clickable pages"
+									buttonHref="https://d-cipher.streamlit.app/"
+									target="_blank"
+									ariaLabel="Check out the D Cipher project"
+								/>
+							</div>
+							<p className="description">CRYPTOGRAPHY TOOLKIT THAT ALLOWS USERS TO ENCRYPT AND DECRYPT THEIR TEXT USING AN ALGORITHM OF THEIR CHOICE.</p>
+						</li>
+		
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="empower clickable pages"
+									buttonHref="https://github.com/KuNull/Custos-Shell"
+									target="_blank"
+									ariaLabel="Check out the Empowershell project"
+								/>
+							</div>
+							<p className="description">POWERSHELL UTILITY SCRIPT THAT COMPARES HASHES TO CHECK THE INTEGRITY OF FILES.</p>
+						</li>
+		
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="hexplorer clickable pages"
+									buttonHref="https://github.com/KuNull/Hexplorer/"
+									target="_blank"
+									ariaLabel="Check out the Hexplorer project"
+								/>
+							</div>
+							<p className="description">COMMAND-LINE UTILITY TO DUMP THE BINARY AND HEXADECIMAL RENDITION OF A FILE.</p>
+						</li>
+		
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="netSleuth clickable pages"
+									buttonHref="https://github.com/KuNull/NetSleuth/"
+									target="_blank"
+									ariaLabel="Check out the NetSleuth project"
+								/>
+							</div>
+							<p className="description">NETWORK SCANNER THAT FINDS ALL THE DEVICES CONNECTED IN THE NETWORK.</p>
+						</li>
+		
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="ossoc clickable pages"
+									buttonHref="https://ossoc.gitbook.io/"
+									target="_blank"
+									ariaLabel="Check out the OpenSOC project"
+								/>
+							</div>
+							<p className="description">PROOF OF CONCEPT FOR SECURITY OPERATIONS CENTRE MADE USING OPEN SOURCE TOOLS.</p>
+						</li>
+		
+		{/*				
+						<li>
+							<h2>
+								<Button
+									button=""
+									buttonClassName="passward clickable pages"
+									buttonHref=""
+									target="_blank"
+								/>
+							</h2>
+							<span className="description">Local password manager with a built-in web server/interface.</span>
+						</li>
+		*/}
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="probe clickable pages"
+									buttonHref="https://github.com/KuNull/Probe"
+									target="_blank"
+									ariaLabel="Check out the Probe project"
+								/>
+							</div>
+							<p className="description">PORTSCANNER THAT IMPLEMENTS MULTITHREADING FOR SWIFT SCANS.</p>
+						</li>
+		
+		{/*
+						<li>
+							<h2>
+								<Button
+									button=""
+									buttonClassName="pyrefox clickable pages"
+									buttonHref="https://github.com/KuNull/PyreFox"
+									target="_blank"
+								/>
+							</h2>
+							<p className="description">user.js file for hardening Firefox browser.</p>
+						</li>
+		*/}
+		
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="signum clickable pages"
+									buttonHref="https://signum-mu.vercel.app/"
+									target="_blank"
+									ariaLabel="Check out the Signum project"
+								/>
+							</div>
+							<p className="description">WEB APP TO GENERATE PASSWORDS AND CHECK THEIR STRENGTH.</p>
+						</li>
+	
+		{/*
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="steghound clickable pages"
+									buttonHref=""
+									target="_blank"
+									ariaLabel="Check out the Steghound project"
+								/>
+							</div>
+							<p className="description">Steganography tool that allows image, text, audio and video steganography.</p>
+						</li>
+		*/}
+	
+		{/*
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="suiteBox clickable pages"
+									buttonHref=""
+									target="_blank"
+									aria-label="Check out the SuiteBox project"
+								/>
+							</div>
+							<p className="description">Collection of Burp Suite extensions to enhace security testing.</p>
+						</li>
+		*/}
+						
+						<li>
+							<div className="capital">
+								<Button
+									button=""
+									buttonClassName="riskore clickable pages"
+									buttonHref="https://riskore.vercel.app/"
+									target="_blank"
+									ariaLabel="Check out the RiSkore project"
+								/>
+							</div>
+							<p className="description">WEB APP TO EVALUATE THE RISK FACED BY AN ORGANIZATION.</p>
+						</li>
+					</ul>
+				</p>
+
+
+									
+				<div className="currentPage section-divider">
 					<span >KUNULL / EXPERIENCE ►</span>
 				</div>
 
