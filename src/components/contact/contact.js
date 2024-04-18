04a99a335365383217f75910053b9b820a5248b3
Kunal W <kunalw2002@gmail.com>
2024-04-18 09:16:09 +0530

    Update contact.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -10,108 +10,26 @@ const Contact = () => {
 				<span>KUNULL / CONTACT ►</span>
 			</div>
 			
-
 			<p className='spacing'>
-	  			<ul className="threeColumnUl">
-	  				<li>
-	  					<div className="capital">
-	  						<Button
-	  							button=""
-	  							buttonClassName="ejpt clickable pages"
-	  							buttonHref="/assets/certifications/eLearnSecurity-Junior-Penetration-Tester.pdf"
-	  							target="_blank"
-	  							aria-label="Check out my EJPT certificate"
-	  						/>
-	  					</div>
-	  					<p className="description">E-LEARN SECURITY JUNIOR PENETRATION TESTER.</p>
-	  				</li>
-	       
-	  {/*
-	  					<li>
-	  						<div className="capital">
-	  							<Button
-	  								button=""
-	  								buttonClassName="cc clickable pages"
-	  								buttonHref=""
-	  								target="_blank"
-	  								ariaLabel="Check out my Certified in Cybersecurity certificate"
-	  							/>
-	  						</div>
-	  						<p className="description">ISC2 Certified in Cybersecurity.</p>
-	  					</li>
-	  */} 
-	  
-	  				<li>
-	  					<div className="capital">
-	  						<Button
-	  							button=""
-	  							buttonClassName="google clickable pages"
-	  							buttonHref="/assets/certifications/Google-Cybersecurity-Professional-Certificate.pdf"
-	  							target="_blank"
-	  							ariaLabel="Check out my Google Cybersecurity Professional certificate"
-	  						/>
-	  					</div>
-	  					<p className="description">GOOGLE CYBERSECURITY PROFESSIONAL CERTIFICATE.</p>
-	  				</li>
-	  
-	  				<li>
-	  					<div className="capital">
-	  						<Button
-	  							button=""
-	  							buttonClassName="icca clickable pages"
-	  							buttonHref="/assets/certifications/INE-Certified-Cloud-Associate.pdf"
-	  							target="_blank"
-	  							ariaLabel="Check out my ICCA certificate"
-	  						/>
-	  					</div>
-	  					<p className="description">INE CERTIFIED CLOUD ASSOCIATE.</p>
-					</li>
-	  
-	  				<li>
-	  					<div className="capital">
-	  						<Button
-	  							button=""
-	  							buttonClassName="nse clickable pages"
-	  							buttonHref="/assets/certifications/NSE-Certification.pdf"
-	  							target="_blank"
-	  							ariaLabel="Check out my NSE certificate"
-	  						/>
-	  					</div>
-	  					<p className="description">FORTINET TRAINING INSTITUTE NETWORK SECURITY EXPERT.</p>
-	  				</li>
-	  
-	  {/*
-	  					<li>
-	  						<div className="capital">
-	  							<Button
-	  								button=""
-	  								buttonClassName="pnpt clickable pages"
-	  								buttonHref=""
-	  								target="_blank"
-	  								ariaLabel="Check out my Practical Network Penetration Tester certificate"
-	  							/>
-	  						</div>
-	  						<p className="description">TCM Security Practical Network Penetration Tester certificate.</p>
-	  					</li>
-	  */}
-	  
-	  	{/*
-	  					<li>
-	  						<div className="capital">
-	  							<Button
-	  								button=""
-	  								buttonClassName="security-plus clickable pages"
-	  								buttonHref=""
-	  								target="_blank"
-	  								ariaLabel="Check out my CompTIA Security+ certificate"
-	  							/>
-	  						</div>
-	  						<p className="description">CompTIA Security+ (SY0-701) certificate.</p>
-	  					</li>
-	  	*/}
-	  
-	  	      		</ul>
+				<span className="personal">SEND ME AN </span>
+				<Button
+					button=""
+					buttonClassName="email clickable pages"
+					buttonHref="mailto:walavalkarkunal@skiff.com"
+					target="_blank"
+					ariaLabel="Send me an e-mail"
+				/>
+				<span className="personal"> IF YOU'D LIKE TO GET IN TOUCH. YOU CAN USE MY PUBLIC </span>
+				<Button
+					button=""
+					buttonClassName="key clickable pages"
+					buttonHref="/assets/key/kunalwalavalkar_public_key.txt"
+					target="_blank"
+					ariaLabel="Use my pgp key"
+				/>
+				<span className="personal"> FOR ENCRYPTION.</span>
 			</p>
+
 		</div>
 	);
 };
