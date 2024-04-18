a57e1c46dc0b529b011ae5931f61c18f345a3eb7
Kunal W <kunalw2002@gmail.com>
2024-04-18 08:41:40 +0530

    Create header.js

---
+++ src/components/header/header.js
@@ -0,0 +1,40 @@
+import Button from '../button/button';
+import '../button/button.css';
+import '../navbar/navbar.css';
+import Link from 'next/link';
+
+const Header = () => {
+	return (
+		<div>
+				<p className='spacing'>
+					<span className="personal">HELLO. I'M </span>
+					<span className="kunalWalavalkar nonClickable pages">
+{/*						
+						<div class="profilepic">
+  							<img src={profilepic} width="200" alt=""/>
+						</div>	
+*/}					
+					</span>	
+{/*	
+					<span className="personal">, AKA </span>
+					<span className="knign nonClickable pages">
+*/}	
+{/*						
+						<div class="profilepic2">
+  							<img src={profilepic2} width="200" alt=""/>
+						</div>	
+					
+     					</span>
+*/}	
+					<span className="personal">, A COMPUTER ENGINEERING AND CYBER SECURITY STUDENT.</span>
+				</p>
+
+		</div>
+	);
+};
+
+Button.defaultProps = {
+	target: '',
+};
+
+export default Header;
